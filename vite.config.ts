1093b7093f0afa55f266f31bd2c465b39ae3276c
Danil Bogdanchikov <bogdanchikovdan@yandex.ru>
2025-12-16 01:07:05 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig, loadEnv, UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  build: { sourcemap: true },
-  base: process.env.VITE_BASE_PATH,
-});
+const getConfig = ({ mode }: { mode: string }): UserConfig => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    build: { sourcemap: true },
+    base: env.VITE_BASE_PATH,
+  };
+};
+
+export default defineConfig(getConfig);
